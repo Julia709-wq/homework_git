bdd5cb3aad6321512245b01e70c96adb5d174d43
Julia <yulia.2018.ivanova@gmail.com>
2024-10-02 00:29:00 +0300

    last fixes

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-def filter_by_state(my_list, state='EXECUTED'):
+
+def filter_by_state(my_list: list, state='EXECUTED') -> list:
+    """Сортирует список по состоянию"""
     filtered_list = []
     for i in my_list:
         if i['state'] == state:
@@ -8,15 +10,11 @@ def filter_by_state(my_list, state='EXECUTED'):
     return filtered_list
 
 
-# print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+def sort_by_date(my_list: list, ascending=True) -> list:
+    """Сортирует список по дате"""
 
-
-def sort_by_date(my_list, ascending=True):
-    # Функция для преобразования строки даты в объект datetime
     def parse_date(date):
+        """Преобразует строку в объект datetime"""
         return datetime.fromisoformat(item['date'])
 
     sorted_list = []
@@ -24,9 +22,3 @@ def sort_by_date(my_list, ascending=True):
         sorted_list = sorted(my_list, key=parse_date, reverse=ascending)
 
     return sorted_list
-
-
-# print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
