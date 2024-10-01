68c8a5aa2b3b8360080dffcf269968e60df32130
Julia <yulia.2018.ivanova@gmail.com>
2024-10-01 23:47:13 +0300

    filter_by_state func

---
+++ src/processing.py
@@ -0,0 +1,14 @@
+def filter_by_state(my_list, state='EXECUTED'):
+    filtered_list = []
+    for i in my_list:
+        if i['state'] == state:
+            filtered_list.append(i)
+    return filtered_list
+
+
+
+
+# print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
