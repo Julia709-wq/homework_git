02994eaf2a487652e4a3c98ecea5099fc902b9c5
Julia <yulia.2018.ivanova@gmail.com>
2024-10-01 23:53:14 +0300

    filter_by_state func

--- src/processing.py
+++ src/processing.py
@@ -8,6 +8,7 @@ def filter_by_state(my_list, state='EXECUTED'):
 
 
 
+
 # print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 #                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 #                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
